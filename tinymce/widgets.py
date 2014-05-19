b601a2d1bb269933d9ac68b6ef1a651994d75f2a
Mohammed Affan <mohammed@thebimhub.com>
2014-05-19 17:16:17 +0400

    document domain before init

--- tinymce/widgets.py
+++ tinymce/widgets.py
@@ -124,7 +124,7 @@ setTimeout(function () {
 }, 0);
 </script>''' % (final_attrs['id'], final_attrs['id'][0:pos], mce_json))
         else:
-            html.append('<script type="text/javascript">tinyMCE.init(%s)</script>' % mce_json)
+            html.append('<script type="text/javascript">document.domain = "thebimhub.com";tinyMCE.init(%s)</script>' % mce_json)
 
         return mark_safe('\n'.join(html))
 
